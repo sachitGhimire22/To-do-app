51a9f4b275700b9db5449b751cc33e71ebe526fa
sachit ghimire <gsachit22@gmail.com>
2024-02-18 02:45:01 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const input_box = document.getElementById("input-box");
+const list_container = document.getElementById("list-container");
+
+
+function addTask() {
+  if (input_box.value === "") {
+    alert("No task Added");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = input_box.value;
+    list_container.appendChild(li);
+    let span = document.createElement("span")
+    span.innerHTML="\u00d7"
+    li.appendChild(span)
+  }
+  input_box.value=""
+}
+
+list_container.addEventListener("click",function(e){
+    if (e.target.tagName === "LI"){
+        e.target.classList.toggle("checked")
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove()
+    }
+},false)
+
+let todo = document.getElementById("todo-app")
